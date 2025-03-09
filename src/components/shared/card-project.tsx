8f97cefa574deb19d0316aea27afb279c08cf233
João Fernando do Amaral Brigido <70164946+joaofbrigido@users.noreply.github.com>
2025-03-09 18:57:19 -0300

    remoção de animaçãoes com react-spring

--- src/components/shared/card-project.tsx
+++ src/components/shared/card-project.tsx
@@ -9,7 +9,6 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import CarouselProject from "./carousel-project";
-import AnimatedContent from "../animations/animated-content";
 
 type CardProjectProps = {
   name: string;
@@ -23,30 +22,28 @@ export const CardProject = ({ name, images, type, year }: CardProjectProps) => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="group cursor-pointer overflow-hidden">
-          <AnimatedContent config={{ tension: 75, friction: 25 }}>
-            <Image
-              src={images[0]}
-              alt={name}
-              width={650}
-              height={650}
-              className="h-[550px] w-[550px] object-cover transition-transform duration-300 group-hover:scale-105 max-lg:h-[400px] max-md:w-full"
-            />
+          <Image
+            src={images[0]}
+            alt={name}
+            width={650}
+            height={650}
+            className="h-[550px] w-[550px] object-cover transition-transform duration-300 group-hover:scale-105 max-lg:h-[400px] max-md:w-full"
+          />
 
-            <h2 className="text-left mt-6 max-md:text-3xl transition-transform duration-300 group-hover:translate-x-2">
-              {name}
-            </h2>
+          <h2 className="text-left mt-6 max-md:text-3xl transition-transform duration-300 group-hover:translate-x-2">
+            {name}
+          </h2>
 
-            <Separator className="my-4" />
+          <Separator className="my-4" />
 
-            <div className="flex justify-between gap-2">
-              <span className="transition-transform duration-300 group-hover:translate-x-2">
-                {type}
-              </span>
-              <span className="transition-transform duration-300 group-hover:-translate-x-2">
-                {year}
-              </span>
-            </div>
-          </AnimatedContent>
+          <div className="flex justify-between gap-2">
+            <span className="transition-transform duration-300 group-hover:translate-x-2">
+              {type}
+            </span>
+            <span className="transition-transform duration-300 group-hover:-translate-x-2">
+              {year}
+            </span>
+          </div>
         </div>
       </DialogTrigger>
       <DialogContent className="min-w-[80%] max-md:max-w-[90%]">
--- src/components/shared/title-page.tsx
+++ src/components/shared/title-page.tsx
@@ -1,12 +1,7 @@
-import React from "react";
-import SplitText from "../animations/split-text";
-
 export const TitlePage = ({ title }: { title: string }) => {
   return (
     <div className="w-full py-8 title-page-gradient mt-16 mb-36">
-      <h1 className="text-8xl max-sm:text-7xl">
-        <SplitText text={title} />
-      </h1>
+      <h1 className="text-8xl max-sm:text-7xl">{title}</h1>
     </div>
   );
 };
